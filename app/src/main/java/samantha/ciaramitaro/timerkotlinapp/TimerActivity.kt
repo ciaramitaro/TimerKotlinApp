5dd11b76eb391473f035a2296e9e4ce7bbeec45f
ciaramitaro <s.n.ciaramitaro@gmail.com>
2019-05-21 07:52:29 -0500

    created prefUtil, and added log to timer activity (will re-model for MVVM later)

--- app/src/main/java/samantha/ciaramitaro/timerkotlinapp/TimerActivity.kt
+++ app/src/main/java/samantha/ciaramitaro/timerkotlinapp/TimerActivity.kt
@@ -1,32 +1,154 @@
 package samantha.ciaramitaro.timerkotlinapp
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_timer.*
+import kotlinx.android.synthetic.main.content_main.*
+import samantha.ciaramitaro.timerkotlinapp.util.PrefUtil
 
 class TimerActivity : AppCompatActivity() {
+
+    enum class TimerState{
+        Stopped, Paused, Running
+    }
+    private lateinit var timer: CountDownTimer
+    private var timerLengthSeconds: Long =0L
+    private var timerState = TimerState.Stopped
+    private var secondsRemaining = 0L
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_timer)
         setSupportActionBar(toolbar)
+        supportActionBar?.setIcon(R.drawable.ic_timer)
+        supportActionBar?.title="   Timer"
 
         fab_stop.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            startTimer()
+            timerState = TimerState.Running
+            updateButtons()
+        }
+        fab_pause.setOnClickListener { view ->
+            timer.cancel()
+            timerState = TimerState.Paused
+            updateButtons()
+        }
+        fab_stop.setOnClickListener { view ->
+            timer.cancel()
+            onTimerFinished()
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        initTimer()
+        //TODO: Remove bakcground timer, hide notification
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if(timerState==TimerState.Running){
+            timer.cancel()
+            //TODO: STart background timer and show notification
+        }
+        else if(timerState==TimerState.Paused){
+            //TODO: SHOW NOTIFICATION
+        }
+        PrefUtil.setPreviousTimerLengthSeconds(timerLengthSeconds, this)
+        PrefUtil.setSecondsRemaining(secondsRemaining, this)
+        PrefUtil.setTimerState(timerState, this)
+    }
+
+    private fun initTimer(){
+        timerState = PrefUtil.getTimerState(this)
+        if(timerState ==TimerState.Stopped){
+            setNewTimerLength()
+        }
+        else {
+            setPreviousTimerLength()
+        }
+        secondsRemaining = if(timerState== TimerState.Running || timerState == TimerState.Paused){
+            PrefUtil.getSecondsRemaining(this)
+        }else
+        { timerLengthSeconds}
+        //TODO: Change secondsRemaining according to background timer stopped
+
+        if(timerState ==TimerState.Running){
+            startTimer()
+        }
+        updateButtons()
+        updateCountdownUI()
+    }
+    private fun onTimerFinished(){
+        timerState= TimerState.Stopped
+
+        setNewTimerLength()
+        progress_countdown.progress=0
+        PrefUtil.setSecondsRemaining(timerLengthSeconds, this)
+        secondsRemaining = timerLengthSeconds
+        updateButtons()
+        updateCountdownUI()
+    }
+    private fun startTimer(){
+        timerState = TimerState.Running
+
+        timer = object : CountDownTimer(secondsRemaining * 1000, 1000) {
+            override fun onFinish() = onTimerFinished()
+
+            override fun onTick(millisUntilFinished: Long) {
+                secondsRemaining = millisUntilFinished / 1000
+                updateCountdownUI()
+            }
+        }.start()
+    }
+    private fun setNewTimerLength(){
+        val lengthInMinutes = PrefUtil.getTimerLength(this)
+        timerLengthSeconds = (lengthInMinutes * 60L)
+        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+    private fun setPreviousTimerLength(){
+        timerLengthSeconds = PrefUtil.getPreviousTimerLengthSeconds(this)
+        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+    private fun updateCountdownUI(){
+        val minutesUntilFinished = secondsRemaining / 60
+        val secondsInMinuteUntilFinished = secondsRemaining - minutesUntilFinished * 60
+        val secondsStr = secondsInMinuteUntilFinished.toString()
+        textView_countdown.text = "$minutesUntilFinished:${if (secondsStr.length == 2) secondsStr else "0" + secondsStr}"
+        progress_countdown.progress = (timerLengthSeconds - secondsRemaining).toInt()
+    }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
-
+    private fun updateButtons(){
+        when (timerState) {
+            TimerState.Running ->{
+                fab_start.isEnabled = false
+                fab_pause.isEnabled = true
+                fab_stop.isEnabled = true
+            }
+            TimerState.Stopped -> {
+                fab_start.isEnabled = true
+                fab_pause.isEnabled = false
+                fab_stop.isEnabled = false
+            }
+            TimerState.Paused -> {
+                fab_start.isEnabled = true
+                fab_pause.isEnabled = false
+                fab_stop.isEnabled = true
+            }
+        }
+    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
